e454afa3bf199cecfc5033407c4381b9ce5a6c36
Alcemir Santos <alcemir.santos@gmail.com>
2016-02-25 15:00:15 +0100

    added the current date to the generated .jar file after building

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -6,10 +6,11 @@ gradle clean
 echo "====================="
 echo "Gradle building..."
 gradle build
+NOW=$(date +%Y%m%d%H%M)
 
 echo "====================="
 echo "Gradle deploying..."
-scp -P 22022 build/libs/repos_crawler-1.0.jar tindao:~/
+scp -P 22022 build/libs/repos_crawler-1.0r$NOW.jar tindao:~/
 scp -P 22022 src/test/resources/target_systems-conflict_prediction.txt tindao:~/
 
 echo "====================="
