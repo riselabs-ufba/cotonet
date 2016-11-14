6e1ce6dc8cb49fe4a409f2246634f20f6aa95ea7
Alcemir R. Santos <alcemir.santos@gmail.com>
2016-11-14 13:41:40 -0300

    small refactoring in the Commit representation.

--- src/main/java/br/com/riselabs/cotonet/model/beans/Commit.java
+++ src/main/java/br/com/riselabs/cotonet/model/beans/Commit.java
@@ -1,3 +1,23 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Alcemir R. Santos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package br.com.riselabs.cotonet.model.beans;
 
 import java.sql.Timestamp;
@@ -11,43 +31,39 @@ public class Commit {
 	private Integer authorID;
 
 	public Commit(String sha1) {
-		this(sha1, null, null, null);
-	}
-
-	public Commit(String sha1, Timestamp date, Integer committer, Integer author) {
-		this(null, sha1, date, committer, author);
+		this(null, sha1, null, null, null);
 	}
 
 	public Commit(Integer id, String sha1, Timestamp date, Integer committer,
 			Integer author) {
 		setID(id);
-		setHash(sha1);
+		setSHA1(sha1);
 		setDatetime(date);
 		setCommitterID(committer);
 		setAuthorID(author);
 	}
 
-	private void setID(Integer id) {
+	public void setID(Integer id) {
 		this.id = id;
 	}
 
-	private void setHash(String sha1) {
+	public void setSHA1(String sha1) {
 		this.sha1 = sha1;
 	}
 
-	private void setDatetime(Timestamp date) {
+	public void setDatetime(Timestamp date) {
 		this.datetime = date;
 	}
 
-	private void setCommitterID(Integer committer) {
+	public void setCommitterID(Integer committer) {
 		this.committerID = committer;
 	}
 
-	private void setAuthorID(Integer author) {
+	public void setAuthorID(Integer author) {
 		this.authorID = author;
 	}
 
-	public String getHash() {
+	public String getSHA1() {
 		return this.sha1;
 	}
 
@@ -67,4 +83,16 @@ public class Commit {
 		return this.id;
 	}
 
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder("[");
+		sb.append(this.id);
+		sb.append("]");
+		sb.append(this.datetime);
+		sb.append("-");
+		sb.append(this.authorID);
+		sb.append(", ");
+		sb.append(this.committerID);
+		return sb.toString();
+	}
 }
--- src/main/java/br/com/riselabs/cotonet/model/dao/validators/CommitValidator.java
+++ src/main/java/br/com/riselabs/cotonet/model/dao/validators/CommitValidator.java
@@ -1,3 +1,23 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Alcemir R. Santos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package br.com.riselabs.cotonet.model.dao.validators;
 
 import br.com.riselabs.cotonet.model.beans.Commit;
@@ -7,7 +27,7 @@ public class CommitValidator implements Validator<Commit> {
 
 	@Override
 	public boolean validate(Commit c) throws InvalidCotonetBeanException {
-		if (c.getHash() == null || c.getHash().length() != 40) {
+		if (c.getSHA1() == null || c.getSHA1().length() != 40) {
 			throw new InvalidCotonetBeanException(
 					Commit.class,
 					"Either the sha1 hash is inexistent, or its size is different than 40.",
@@ -17,10 +37,10 @@ public class CommitValidator implements Validator<Commit> {
 			throw new InvalidCotonetBeanException(Commit.class,
 					"Commit date is inexistent.", new NullPointerException());
 		}
-		if (c.getCommitterID() == null || c.getAuthorID() == null) {
+		if ( c.getAuthorID() == null) {
 			throw new InvalidCotonetBeanException(
 					Commit.class,
-					"Either the committer ID, the author ID, or both are unavailable.",
+					"The author ID is unavailable.",
 					new NullPointerException());
 		}
 		return true;
